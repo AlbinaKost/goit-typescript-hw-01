4469abadee2cb31bed6b61cf41cb74a7d1f3af85
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-05-03 15:08:35 +0300

    upd

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,26 +1,14 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// async function fetchData(url) {
-//   try {
-//     const response = await axios.get(url);
-//     return response.data;
-//   } catch (error) {
-//     throw new Error(`Error fetching from ${url}: ${error}`);
-//   }
-// }
+async function fetchData<T>(url : string) : Promise<T> {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
 
 // Типізуйте асинхронну функцію fetchData, яка приймає URL ресурсу та повертає об'єкт з даними.
 //  Використовуйте Generics для типізації повернутих даних.
 
-const promise: Promise<string> = new Promise((resolve) => {
-    setInterval(() => {
-      resolve('Done!');
-    }, 1000);
-  });
-  
-  promise.then((data) => {
-    console.log(data);
-  });
-  
-  export {};
-  
